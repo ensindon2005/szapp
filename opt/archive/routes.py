f4046dd1bbb5802cb2d9d7ccd877f1748a9840b6
ensindon2005 <edinsonrivera@gmail.com>
2019-04-12 16:33:11 +0200

    working table

--- opt/archive/routes.py
+++ opt/archive/routes.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from io import BytesIO,StringIO
 from flask_login import login_user, current_user, logout_user, login_required
 from flask import (render_template, url_for, flash, redirect, request, 
@@ -6,52 +7,89 @@ from flask import (render_template, url_for, flash, redirect, request,
 from opt.users.utils import *
 from werkzeug import secure_filename
 from opt.archive.utils import allowed_file
+from opt.errors.routes import *
 from opt.models import *
 from opt import db
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+from os.path import join, dirname, realpath
 
 archives=Blueprint('archives',__name__)
 
-#UPLOADS_PATH = join(dirname(realpath(__file__)), './static/uploads/')
-#UPLOADS_FOLDER =  current_app.config['UPLOAD_FOLDER']
+excel=['xls','xlsx']
+csv=['csv','txt']
+encodes = ["utf8", "cp1252"]
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 @archives.route('/list', methods=['GET','POST'])
 @login_required
-def listfiles():
+def list_files():
   folder=current_user.username
-  files=os.listdir(current_app.config['UPLOAD_FOLDER']+folder)
-
+  files=os.listdir(current_app.config['UPLOAD_FOLDER']+current_user.username)
   
   return render_template('list.html',folder=folder, files=files)
+
+@archives.route('/download/<filename>')
+@login_required
+def download(filename):
+        # no best solution but it is working
+        fbase= os.path.join('./static/uploads/',current_user.username,filename)
+        #ruta=os.path.abspath(filename)
+        #file=os.path.join(path,filename)
+        #file=os.path.join(current_app.config['UPLOAD_FOLDER'],current_user.username,filename)
+        #return send_file(filename=filename, as_attachment=True)
+        return send_file(fbase,filename,as_attachment=True)
+  
+
+  
+
 
 @archives.route('/pandas/<filename>', methods=['GET','POST'])
 @login_required
 def pandas(filename):
-    path=os.path.join(current_app.config['UPLOAD_FOLDER'],current_user.username)
-    df=pd.read_excel(path+'/'+filename)
+    path_file=os.path.join(current_app.config['UPLOAD_FOLDER'],current_user.username,filename)
+    extension=filename.split('.')[-1]
+    if extension in excel:
+        #df=pd.read_excel(path) 
+       df=pd.read_excel(path_file,index_col=False)
+
+    elif extension in csv:
+       try:
+            df=pd.read_csv(path_file, sep=';', encoding=encodes[0],index_col=False)
+            
+       except:
+            df=pd.read_csv(path_file,sep=';',encoding=encodes[1],index_col=False)
+            
+    else:
+          flash(f'I can not work this format', 'danger')
     
-    #df.plot(kind='bar',x='modelo',y='price')
-    headers=df.columns.values      
-    rows=df.itertuples(index=False)  
+    headers=df.columns.values
+    before=list(df.dtypes)
+    after=[]
+    for header in headers:
+         if df[header].dtype == object:
+            df[header] = df[header].astype(str)
+            a='string'            
+         else:
+            a = 'number'
+         after.append(a)
+    rows=df.itertuples(index=False)
 
-   
-    #df=pd.read_excel('futures.xlsx')
     #return render_template('pandas1.html',data=df.itertuples())
-    return render_template('pandas1.html',data=rows,headers=headers)
-    #a=plt.show()
-    #return render_template('pandas.html', bild=a,title='excel', 
-                     #   tables=[df.to_html(classes='data', index=False)], titles=df.columns.values)
+    return render_template('pandas1.html',data=rows,headers=headers,dtypes=after,df=df)
 
-
-
+@archives.route('/format/<filename>', methods=['GET','POST'])
+def format_file(filename):
+      formats={'String':'string', 'number':'number',}
+      return 'to format columns'
+   
 
 @archives.route('/upload', methods= ['GET','POST'])
 @login_required
 def upload():
   if request.method == 'POST':
+        folder=current_user.username
     # check if the post request has the file part
         if 'inputfile' not in request.files:
             flash('Please add a file!','danger')
@@ -66,12 +104,13 @@ def upload():
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            folder=current_user.username
             file.save(os.path.join(current_app.config['UPLOAD_FOLDER'],folder,filename))
             
             return redirect(url_for('archives.uploaded_file',filename=filename))
         
   return render_template('upload.html')
+
+
 
 @archives.route('/uploaded/<filename>', methods=['GET'])
 def uploaded_file(filename):
